32ebebfcb2eaab82bc36e64a11cf81214f15a7a5
Lelo1818 <34315392+Lelo1818@users.noreply.github.com>
2025-10-26 19:45:45 -0300

    Simplify response message in hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    status: "ativo",
-    message: "✅ Motor Essentia rodando perfeitamente no Vercel!"
-  });
+  res.status(200).json({ message: "✅ Essentia ativo no Vercel!" });
 }
